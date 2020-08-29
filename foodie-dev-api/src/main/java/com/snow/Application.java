d1eb90239e93344d307e2b10ffd30376b6fe35c5
snow <wjl7123093@163.com>
2020-08-29 18:43:45 +0800

    update. 新增了 @MapperScan() 扫描通用 mapper 所在的包。20200829

--- foodie-dev-api/src/main/java/com/snow/Application.java
+++ foodie-dev-api/src/main/java/com/snow/Application.java
@@ -2,7 +2,10 @@ package com.snow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+// 扫描 mybatis 通用 mapper 所在的包
+@MapperScan(basePackages = "com.snow.mapper")
 @SpringBootApplication
 public class Application {
 
