8d58e975fc7d89ae1be645fc9745d3f236a8e795
wjl7123093 <wjl7123093@163.com>
2020-08-30 21:22:27 +0800

    create. 跨域访问配置。2020.8.30

---
+++ foodie-dev-api/src/main/java/com/snow/config/CorsConfig.java
@@ -0,0 +1,43 @@
+package com.snow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Desc: 跨域访问配置
+ */
+@Configuration
+public class CorsConfig {
+
+    public CorsConfig() {
+    }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1. 添加 cors 配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080");
+
+        // 设置是否发送 cookie 信息
+        config.setAllowCredentials(true);
+
+        // 设置允许请求的方式
+        config.addAllowedMethod("*");
+
+        // 设置允许的 header
+        config.addAllowedHeader("*");
+
+        // 2. 为 url 添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", config);
+
+        // 3. 返回重新定义好的 corsSource
+        return new CorsFilter(corsSource);
+
+    }
+
+}
