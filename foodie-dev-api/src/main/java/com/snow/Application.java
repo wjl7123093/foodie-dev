89c26f045818d2305afbc24e4cddfdc043d45185
wjl7123093 <wjl7123093@163.com>
2020-08-30 21:26:19 +0800

    update. 新增了 @ComponentScan 注解（扫描所有包以及相关组件包）。2020.8.30

--- foodie-dev-api/src/main/java/com/snow/Application.java
+++ foodie-dev-api/src/main/java/com/snow/Application.java
@@ -2,10 +2,13 @@ package com.snow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.snow.mapper")
+// 扫描所有包以及相关组件包
+@ComponentScan(basePackages = {"com.snow", "org.n3r.idworker"})
 @SpringBootApplication
 public class Application {
 
