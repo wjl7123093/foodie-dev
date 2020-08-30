07e4efed24c0e652a82759b7cadd3a5c1892ec2f
wjl7123093 <wjl7123093@163.com>
2020-08-30 21:20:38 +0800

    update. 新增了 @ApiIgnore 注解（测试接口，Api文档不显示）。2020.8.30

--- foodie-dev-api/src/main/java/com/snow/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/snow/controller/HelloController.java
@@ -2,7 +2,10 @@ package com.snow.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+// Api 文档不显示
+@ApiIgnore
 @RestController
 public class HelloController {
 
