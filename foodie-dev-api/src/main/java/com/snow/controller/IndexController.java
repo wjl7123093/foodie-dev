774547214f518d304b3003eac94f2c491d3bfb77
snow <wjl7123093@163.com>
2020-08-31 14:31:51 +0800

    create. 首页 Controller。2020.8.31

---
+++ foodie-dev-api/src/main/java/com/snow/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.snow.controller;
+
+import com.snow.enums.YesOrNo;
+import com.snow.pojo.Carousel;
+import com.snow.service.CarouselService;
+import com.snow.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(value = "首页", tags = {"首页展示的相关接口"})
+@RestController
+@RequestMapping("index")
+public class IndexController {
+
+    @Autowired
+    public CarouselService carouselService;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public IMOOCJSONResult carousel() {
+        List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+        return IMOOCJSONResult.ok(list);
+    }
+
+}
