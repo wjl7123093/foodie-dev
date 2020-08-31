2cd547ddcaf654b126598ff650e6f57127fb0b2a
wjl7123093 <wjl7123093@163.com>
2020-08-31 22:52:07 +0800

    update. 新增了 mapper 中搜索商品列表的接口 & 配置。2020.8.31

--- foodie-dev-mapper/src/main/java/com/snow/mapper/ItemsMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/snow/mapper/ItemsMapperCustom.java
@@ -3,6 +3,7 @@ package com.snow.mapper;
 import com.snow.my.mapper.MyMapper;
 import com.snow.pojo.Items;
 import com.snow.pojo.vo.ItemCommentVO;
+import com.snow.pojo.vo.SearchItemVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -11,5 +12,7 @@ import java.util.Map;
 public interface ItemsMapperCustom {
 
     public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
+
+    public List<SearchItemVO > searchItems(@Param("paramsMap") Map<String, Object> map);
 
 }
