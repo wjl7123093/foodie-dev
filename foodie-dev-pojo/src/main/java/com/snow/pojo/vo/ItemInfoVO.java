c3b6a867a51868013915cdad8d7ae90541140007
snow <wjl7123093@163.com>
2020-08-31 17:56:34 +0800

    create. 商品详情 VO。2020.8.31

---
+++ foodie-dev-pojo/src/main/java/com/snow/pojo/vo/ItemInfoVO.java
@@ -0,0 +1,51 @@
+package com.snow.pojo.vo;
+
+import com.snow.pojo.Items;
+import com.snow.pojo.ItemsImg;
+import com.snow.pojo.ItemsParam;
+import com.snow.pojo.ItemsSpec;
+
+import java.util.List;
+
+/**
+ * 商品详情 VO
+ */
+public class ItemInfoVO {
+
+    private Items item;
+    private List<ItemsImg> itemImgList;
+    private List<ItemsSpec> itemSpecList;
+    private ItemsParam itemParams;
+
+    public Items getItem() {
+        return item;
+    }
+
+    public void setItem(Items item) {
+        this.item = item;
+    }
+
+    public List<ItemsImg> getItemImgList() {
+        return itemImgList;
+    }
+
+    public void setItemImgList(List<ItemsImg> itemImgList) {
+        this.itemImgList = itemImgList;
+    }
+
+    public List<ItemsSpec> getItemSpecList() {
+        return itemSpecList;
+    }
+
+    public void setItemSpecList(List<ItemsSpec> itemSpecList) {
+        this.itemSpecList = itemSpecList;
+    }
+
+    public ItemsParam getItemParams() {
+        return itemParams;
+    }
+
+    public void setItemParams(ItemsParam itemParams) {
+        this.itemParams = itemParams;
+    }
+}
