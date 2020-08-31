be6152fb688abfa737a57197bfafcba9ec0e14b4
snow <wjl7123093@163.com>
2020-08-31 17:34:51 +0800

    update. 新增 @Transaction 注解。2020.8.31

--- foodie-dev-service/src/main/java/com/snow/service/impl/CarouselServiceImpl.java
+++ foodie-dev-service/src/main/java/com/snow/service/impl/CarouselServiceImpl.java
@@ -6,6 +6,8 @@ import com.snow.pojo.Carousel;
 import com.snow.service.CarouselService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public class CarouselServiceImpl implements CarouselService {
     @Autowired
     public CarouselMapper carouselMapper;
 
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public List<Carousel> queryAll(Integer isShow) {
 
