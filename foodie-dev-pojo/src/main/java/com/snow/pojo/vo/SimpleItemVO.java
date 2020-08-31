a3adc83fc3eea749c2d0c3adb8d2f1625b5eca69
snow <wjl7123093@163.com>
2020-08-31 17:01:31 +0800

    create. 6个最新简单商品数据类型 VO。2020.8.31

---
+++ foodie-dev-pojo/src/main/java/com/snow/pojo/vo/SimpleItemVO.java
@@ -0,0 +1,37 @@
+package com.snow.pojo.vo;
+
+import java.util.List;
+
+/**
+ * 6个最新简单商品数据类型 VO
+ */
+public class SimpleItemVO {
+
+    private String itemId;
+    private String itemName;
+    private String itemUrl;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+}
