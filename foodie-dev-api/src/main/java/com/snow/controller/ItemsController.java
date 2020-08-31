9a2976857d319ded5e892e31603a8cd2c54afdd5
wjl7123093 <wjl7123093@163.com>
2020-08-31 22:52:57 +0800

    update. 新增了搜索商品列表路由。2020.8.31

--- foodie-dev-api/src/main/java/com/snow/controller/ItemsController.java
+++ foodie-dev-api/src/main/java/com/snow/controller/ItemsController.java
@@ -88,7 +88,40 @@ public class ItemsController extends BaseController {
             pageSize = COMMENT_PAGE_SIZE;
         }
 
-        PagedGridResult grid = itemService.queryPagedComments(itemId, level, page, pageSize);
+        PagedGridResult grid = itemService.queryPagedComments(itemId,
+                                                                level,
+                                                                page,
+                                                                pageSize);
+
+        return IMOOCJSONResult.ok(grid);
+    }
+
+    @ApiOperation(value = "搜索商品列表", notes = "搜索商品列表", httpMethod = "GET")
+    @GetMapping("/search")
+    public IMOOCJSONResult search(
+            @ApiParam(name = "keywords", value = "关键字", required = true)
+            @RequestParam String keywords,
+            @ApiParam(name = "sort", value = "排序", required = false)
+            @RequestParam String sort,
+            @ApiParam(name = "page", value = "查询下一页的第几页", required = false)
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = false)
+            @RequestParam Integer pageSize) {
+
+        if (StringUtils.isBlank(keywords)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        if (page == null) {
+            page = 1;
+        }
+        if (pageSize == null) {
+            pageSize = PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.searchItems( keywords,
+                                                        sort,
+                                                        page,
+                                                        pageSize);
 
         return IMOOCJSONResult.ok(grid);
     }
