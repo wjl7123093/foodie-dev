4a60fafa110a7933167d478d605568e4215df82e
snow <wjl7123093@163.com>
2020-09-01 11:03:42 +0800

    update. mapper 中新增购物车接口 & 配置。2020.9.1

--- foodie-dev-mapper/src/main/java/com/snow/mapper/ItemsMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/snow/mapper/ItemsMapperCustom.java
@@ -4,6 +4,7 @@ import com.snow.my.mapper.MyMapper;
 import com.snow.pojo.Items;
 import com.snow.pojo.vo.ItemCommentVO;
 import com.snow.pojo.vo.SearchItemVO;
+import com.snow.pojo.vo.ShopcartVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -16,5 +17,7 @@ public interface ItemsMapperCustom {
     public List<SearchItemVO > searchItems(@Param("paramsMap") Map<String, Object> map);
 
     public List<SearchItemVO > searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
+
+    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
 
 }
