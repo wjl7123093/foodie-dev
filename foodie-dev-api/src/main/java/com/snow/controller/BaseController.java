4f6825691c2d2e2fc779c565972dc52313189e15
snow <wjl7123093@163.com>
2020-09-01 18:19:50 +0800

    update. 新增前端 cookie 名配置。2020.9.1

--- foodie-dev-api/src/main/java/com/snow/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/snow/controller/BaseController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BaseController {
 
+    // 前端 cookie 名
+    public static final String FOODIE_SHOP_CART = "shopcart";
+
     public static final Integer COMMENT_PAGE_SIZE = 10;
 
     public static final Integer PAGE_SIZE = 20;
