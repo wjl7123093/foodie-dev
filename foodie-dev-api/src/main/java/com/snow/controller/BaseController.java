e19ba6b6e8dfd8359878cf9f1a66dde081466f3a
snow <wjl7123093@163.com>
2020-09-02 11:24:31 +0800

    update. 新增支付回调 URL。2020.9.2

--- foodie-dev-api/src/main/java/com/snow/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/snow/controller/BaseController.java
@@ -12,4 +12,11 @@ public class BaseController {
 
     public static final Integer PAGE_SIZE = 20;
 
+    // 支付中心回调 url
+    String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
+
+    // 微信支付成功 -> 支付中心 -> 天天吃货平台
+    //                        |-> 回调通知 url
+    String payReturnUrl = "http://localhost:8088/orders/notifyMerchantOrderPaid";
+
 }
