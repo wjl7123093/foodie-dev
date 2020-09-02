6645dcb083ba2dc2c006462998aa7cfa1be5005d
snow <wjl7123093@163.com>
2020-09-02 13:33:01 +0800

    update. 新增 @EnableScheduling 注解。2020.9.2

--- foodie-dev-api/src/main/java/com/snow/Application.java
+++ foodie-dev-api/src/main/java/com/snow/Application.java
@@ -3,12 +3,15 @@ package com.snow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.snow.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.snow", "org.n3r.idworker"})
+// 开启定时任务
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
