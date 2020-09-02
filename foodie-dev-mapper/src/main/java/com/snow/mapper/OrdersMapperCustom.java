acbd08d606db38e473100342475878065dc54e1a
wjl7123093 <wjl7123093@163.com>
2020-09-02 22:46:05 +0800

    create. 用户中心我的订单 mapper 接口 & 配置。2020.9.2

---
+++ foodie-dev-mapper/src/main/java/com/snow/mapper/OrdersMapperCustom.java
@@ -0,0 +1,15 @@
+package com.snow.mapper;
+
+import com.snow.my.mapper.MyMapper;
+import com.snow.pojo.Orders;
+import com.snow.pojo.vo.MyOrdersVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OrdersMapperCustom {
+
+    public List<MyOrdersVO> queryMyOrders(@Param("paramsMap") Map<String, Object> map);
+
+}
