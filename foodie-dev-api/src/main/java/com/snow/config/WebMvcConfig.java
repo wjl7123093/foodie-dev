e3e43ee9c916a0caabee7c6de72a61f5a5f22b0b
snow <wjl7123093@163.com>
2020-09-02 17:54:34 +0800

    update. 新增静态资源映射。2020.9.2

--- foodie-dev-api/src/main/java/com/snow/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/snow/config/WebMvcConfig.java
@@ -4,12 +4,22 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Web 其他配置
  */
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 实现静态资源的映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/") // 映射 swagger2
+                .addResourceLocations("file:/workspaces/images/");   // 映射本地静态资源
+    }
 
     // RestTemplate 实体
     @Bean
