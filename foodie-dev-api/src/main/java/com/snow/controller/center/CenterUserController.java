0950b0f335714f3504a0e95b85f7d4ca65f16d75
snow <wjl7123093@163.com>
2020-09-02 17:57:05 +0800

    update. 新增上传用户头像路由。2020.9.2

--- foodie-dev-api/src/main/java/com/snow/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/snow/controller/center/CenterUserController.java
@@ -1,22 +1,29 @@
 package com.snow.controller.center;
 
+import com.snow.controller.BaseController;
 import com.snow.pojo.Users;
 import com.snow.pojo.bo.center.CenterUserBO;
+import com.snow.resource.FileUpload;
 import com.snow.service.center.CenterUserService;
 import com.snow.utils.CookieUtils;
+import com.snow.utils.DateUtil;
 import com.snow.utils.IMOOCJSONResult;
 import com.snow.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +35,103 @@ public class CenterUserController {
 
     @Autowired
     public CenterUserService centerUserService;
+
+    @Autowired
+    public FileUpload fileUpload;
+
+    @ApiOperation(value = "上传用户头像", notes = "上传用户头像", httpMethod = "POST")
+    @PostMapping("/uploadFace")
+    public IMOOCJSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户 ID", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "头像文件", required = true)
+            MultipartFile file,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        // 定义头像保存的地址
+//        String fileSpace = IMAGE_USER_FACE_LOCATION;
+        String fileSpace = fileUpload.getImageUserFaceLocation();
+        // 在路径上为每一个用户增加一个userid，用于区分不同用户上传
+        String uploadPathPrefix = File.separator + userId;
+
+        // 开始文件上传
+        if (file != null) {
+            FileOutputStream fileOutputStream = null;
+            try {
+                // 获得文件上传的文件名称
+                String fileName = file.getOriginalFilename();
+
+                if (StringUtils.isNotBlank(fileName)) {
+
+                    // 文件重命名  imooc-face.png -> ["imooc-face", "png"]
+                    String fileNameArr[] = fileName.split("\\.");
+
+                    // 获取文件的后缀名
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+
+                    // 文件格式限制，防止黑客传入其他类型文件
+                    if (!suffix.equalsIgnoreCase("png") &&
+                            !suffix.equalsIgnoreCase("jpg") &&
+                            !suffix.equalsIgnoreCase("jpeg") ) {
+                        return IMOOCJSONResult.errorMsg("图片格式不正确！");
+                    }
+
+                    // face-{userid}.png
+                    // 文件名称重组 覆盖式上传，增量式：额外拼接当前时间
+                    String newFileName = "face-" + userId + "." + suffix;
+
+                    // 上传的头像最终保存的位置
+                    String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+                    // 用于提供给web服务访问的地址
+                    uploadPathPrefix += ("/" + newFileName);
+
+                    File outFile = new File(finalFacePath);
+                    if (outFile.getParentFile() != null) {
+                        // 创建文件夹
+                        outFile.getParentFile().mkdirs();
+                    }
+
+                    // 文件输出保存到目录
+                    fileOutputStream = new FileOutputStream(outFile);
+                    InputStream inputStream = file.getInputStream();
+                    IOUtils.copy(inputStream, fileOutputStream);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fileOutputStream != null) {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            return IMOOCJSONResult.errorMsg("头像文件不能为空！");
+        }
+
+        // 获取图片服务地址
+        String imageServerUrl = fileUpload.getImageServerUrl();
+
+        // 由于浏览器可能存在缓存的情况，所以在这里，我们需要加上时间戳来保证更新后的图片可以及时刷新
+        String finalUserFaceUrl = imageServerUrl + uploadPathPrefix
+                + "?t=" + DateUtil.getCurrentDateString(DateUtil.DATE_PATTERN);
+
+        // 更新用户头像到数据库
+        Users userResult = centerUserService.updateUserFace(userId, finalUserFaceUrl);
+
+        userResult = setNullProperty(userResult);
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+        // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
+
+        return IMOOCJSONResult.ok();
+    }
 
     @ApiOperation(value = "获取用户信息", notes = "获取用户信息", httpMethod = "GET")
     @GetMapping("/userInfo")
