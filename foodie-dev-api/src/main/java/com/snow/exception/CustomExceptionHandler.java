35919253bd424fd01c3a2605fc11c6a57242f271
snow <wjl7123093@163.com>
2020-09-03 11:27:08 +0800

    update. 经测试，捕获到了异常，但返回的 IMOOCJSONResult 并未被前端获取到，所以没有弹出提示信息。2020.9.3

--- foodie-dev-api/src/main/java/com/snow/exception/CustomExceptionHandler.java
+++ foodie-dev-api/src/main/java/com/snow/exception/CustomExceptionHandler.java
@@ -16,7 +16,9 @@ public class CustomExceptionHandler {
     // 上传文件超过 500k，捕获异常：MaxUploadSizeExceededException
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public IMOOCJSONResult handlerMaxUploadFile(MaxUploadSizeExceededException excepiton) {
-        return IMOOCJSONResult.errorMsg("文件上传大小不能超过500k，请压缩图片或降低图片质量后再上传！");
+        // 捕获到了，也进来了。就是返回的 IMOOCJSONResult 并不会弹出 alertDialog
+        System.out.println("1 - 文件上传大小不能超过500k，请压缩图片或降低图片质量后再上传！");
+        return IMOOCJSONResult.errorMsg("2 - 文件上传大小不能超过500k，请压缩图片或降低图片质量后再上传！");
     }
 
 }
