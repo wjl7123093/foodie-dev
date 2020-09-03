92fc4dd584cac3cbb0ec67f609afd6734a36b25e
snow <wjl7123093@163.com>
2020-09-03 11:21:13 +0800

    update. 新增更新订单状态 & 删除订单接口。2020.9.3

--- foodie-dev-service/src/main/java/com/snow/service/center/MyOrdersService.java
+++ foodie-dev-service/src/main/java/com/snow/service/center/MyOrdersService.java
@@ -1,5 +1,6 @@
 package com.snow.service.center;
 
+import com.snow.pojo.Orders;
 import com.snow.pojo.Users;
 import com.snow.pojo.bo.center.CenterUserBO;
 import com.snow.utils.PagedGridResult;
@@ -21,4 +22,31 @@ public interface MyOrdersService {
                                          Integer orderStatus,
                                          Integer page,
                                          Integer pageSize);
+
+    /**
+     * 更新订单状态 -> 商家发货（模拟）
+     * @param orderId
+     */
+    public void updateDeliverOrderStatus(String orderId);
+
+    /**
+     * 查询我的订单
+     * @param userId
+     * @param orderId
+     * @return
+     */
+    public Orders queryMyOrder(String userId, String orderId);
+
+    /**
+     * 更新订单状态 -> 确认收货
+     * @param orderId
+     */
+    public boolean updateReceiveOrderStatus(String orderId);
+
+    /**
+     * 删除订单（逻辑删除）
+     * @param userId
+     * @param orderId
+     */
+    public boolean deleteOrder(String userId, String orderId);
 }
--- foodie-dev-service/src/main/java/com/snow/service/impl/center/MyOrdersServiceImpl.java
+++ foodie-dev-service/src/main/java/com/snow/service/impl/center/MyOrdersServiceImpl.java
@@ -2,7 +2,13 @@ package com.snow.service.impl.center;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.snow.enums.OrderStatusEnum;
+import com.snow.enums.YesOrNo;
+import com.snow.mapper.OrderStatusMapper;
+import com.snow.mapper.OrdersMapper;
 import com.snow.mapper.OrdersMapperCustom;
+import com.snow.pojo.OrderStatus;
+import com.snow.pojo.Orders;
 import com.snow.pojo.vo.MyOrdersVO;
 import com.snow.service.center.MyOrdersService;
 import com.snow.utils.PagedGridResult;
@@ -10,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +28,12 @@ public class MyOrdersServiceImpl implements MyOrdersService {
 
     @Autowired
     public OrdersMapperCustom ordersMapperCustom;
+
+    @Autowired
+    public OrdersMapper ordersMapper;
+
+    @Autowired
+    public OrderStatusMapper orderStatusMapper;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -36,6 +50,72 @@ public class MyOrdersServiceImpl implements MyOrdersService {
         List<MyOrdersVO> list = ordersMapperCustom.queryMyOrders(paramsMap);
 
         return setterPagedGrid(list, page);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateDeliverOrderStatus(String orderId) {
+
+        OrderStatus updateOrder = new OrderStatus();
+        updateOrder.setOrderStatus(OrderStatusEnum.WAIT_RECEIVE.type);
+        updateOrder.setDeliverTime(new Date());
+
+        Example orderStatusExp = new Example(OrderStatus.class);
+        Example.Criteria criteria = orderStatusExp.createCriteria();
+        criteria.andEqualTo("orderId", orderId);
+        criteria.andEqualTo("orderStatus", OrderStatusEnum.WAIT_DELIVER.type);
+
+        orderStatusMapper.updateByExampleSelective(updateOrder, orderStatusExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Orders queryMyOrder(String userId, String orderId) {
+
+        Orders queryOrder = new Orders();
+        queryOrder.setUserId(userId);
+        queryOrder.setId(orderId);
+        queryOrder.setIsDelete(YesOrNo.NO.type);
+
+        return ordersMapper.selectOne(queryOrder);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public boolean updateReceiveOrderStatus(String orderId) {
+
+        // 更新的状态信息
+        OrderStatus updateStatus = new OrderStatus();
+        updateStatus.setOrderStatus(OrderStatusEnum.SUCCESS.type);
+        updateStatus.setSuccessTime(new Date());
+
+        // 查询条件
+        Example example = new Example(OrderStatus.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("orderId", orderId);
+        criteria.andEqualTo("orderStatus", OrderStatusEnum.WAIT_RECEIVE.type);
+
+        int result = orderStatusMapper.updateByExampleSelective(updateStatus, example);
+
+        return result == 1 ? true : false;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public boolean deleteOrder(String userId, String orderId) {
+
+        Orders delOrder = new Orders();
+        delOrder.setIsDelete(YesOrNo.YES.type);
+        delOrder.setUpdatedTime(new Date());
+
+        Example example = new Example(Orders.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("userId", userId);
+        criteria.andEqualTo("id", orderId);
+
+        int result = ordersMapper.updateByExampleSelective(delOrder, example);
+
+        return result == 1 ? true : false;
     }
 
     /**
