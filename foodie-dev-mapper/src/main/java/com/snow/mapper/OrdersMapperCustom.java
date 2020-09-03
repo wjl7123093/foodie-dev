a6e2e743f6f24e03e118d5a56f50c138e9b3ce33
snow <wjl7123093@163.com>
2020-09-03 16:26:10 +0800

    create. mapper 中新增查询订单状态数 & 订单动向 接口 & 配置。2020.9.3

--- foodie-dev-mapper/src/main/java/com/snow/mapper/OrdersMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/snow/mapper/OrdersMapperCustom.java
@@ -1,6 +1,7 @@
 package com.snow.mapper;
 
 import com.snow.my.mapper.MyMapper;
+import com.snow.pojo.OrderStatus;
 import com.snow.pojo.Orders;
 import com.snow.pojo.vo.MyOrdersVO;
 import org.apache.ibatis.annotations.Param;
@@ -11,5 +12,9 @@ import java.util.Map;
 public interface OrdersMapperCustom {
 
     public List<MyOrdersVO> queryMyOrders(@Param("paramsMap") Map<String, Object> map);
+
+    public int getMyOrderStatusCounts(@Param("paramsMap") Map<String, Object> map);
+
+    public List<OrderStatus> getMyOrderTrend(@Param("paramsMap") Map<String, Object> map);
 
 }
